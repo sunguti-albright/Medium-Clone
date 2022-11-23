720564d99131cc5ad92a43d2cf5d78d7a3fcb9b0
AlbrightSunguti <albright.sunguti@abxtractventurelabs.com>
2022-11-23 14:18:21 +0300

    create register action

--- Desktop/medium-clone/src/app/auth/store/actions.ts
+++ Desktop/medium-clone/src/app/auth/store/actions.ts
@@ -1,7 +1,12 @@
 import { createAction, props } from '@ngrx/store';
+import { RegisterRequestInterface } from '../types/registerRequestInterface';
 import { ActionTypes } from './actionTypes';
 
 export const registerAction = createAction(
     ActionTypes.REGISTER,
-  props<{ username: string; email:string; password: string }>()
+  props<{ request: RegisterRequestInterface }>()
 );
+export const loginaAtion = createAction(
+  ActionTypes.LOGIN,
+  props<{username:string; password:string}>()
+)
