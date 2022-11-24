01ce80db9395ffe6e188574df1569022f442e4b8
AlbrightSunguti <albright.sunguti@abxtractventurelabs.com>
2022-11-24 10:25:18 +0300

    add service to app module

--- Desktop/medium-clone/src/app/app.module.ts
+++ Desktop/medium-clone/src/app/app.module.ts
@@ -5,20 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { StoreModule } from '@ngrx/store';
-
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AuthModule,
-    StoreModule.forRoot({}, {})
+    StoreModule.forRoot({}), //.forRoot(reducers)
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production,
+    }),
+    HttpClientModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
