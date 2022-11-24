538b13671b552c5703be912291481c1598e612b5
AlbrightSunguti <albright.sunguti@abxtractventurelabs.com>
2022-11-24 10:26:14 +0300

    action types

--- Desktop/medium-clone/src/app/auth/store/actionTypes.ts
+++ Desktop/medium-clone/src/app/auth/store/actionTypes.ts
@@ -1,5 +1,8 @@
-export enum ActionTypes{
-    REGISTER = '[Auth] Register',
-    REGISTER_SUCCESS = '[Auth] Register success',
-    REGISTER_FAILURE = '[Auth] Register failure'
+export enum ActionTypes {
+  REGISTER = '[Auth] Register',
+  REGISTER_SUCCESS = '[Auth] Register success',
+  REGISTER_FAILURE = '[Auth] Register failure',
+  LOGIN = '[Auth] Login',
+  LOGIN_SUCCESS = '[Auth] login success',
+  LOGIN_FAILURE = '[Auth] Login failure',
 }
